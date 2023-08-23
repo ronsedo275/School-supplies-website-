b3a36eeb4e559e0470ca2c22d8b49019a89f2718
lisedo123 <138423672+lisedo123@users.noreply.github.com>
2023-08-22 22:01:35 -0400

    commit

---
+++ Donor-Page/donor.js
@@ -0,0 +1,23 @@
+
+const names = document.getElementsByClassName("drives");
+const cheap = document.getElementsByClassName("supplies");
+const map = document.getElementsByClassName("maps");
+
+
+
+
+function drives(){
+    names.style.display = "100px";
+    cheap.style.display = "0px";
+    map.style.display = "0px";
+}
+// function cheap(){
+//     driveName.style.left = "-400px";
+//     cheapSupplies.style.left = "50px";
+//     map.style.left = "-710px";
+// }
+// function maps(){
+//     driveName.style.left = "-400px";
+//     cheapSupplies.style.left = "50px";
+//     map.style.left = "-710px";
+// }
