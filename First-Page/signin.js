4ed0076e0425077c0d7b25e402de1b74f89767ea
lilipod <lisedo147@gmail.com>
2023-10-10 12:32:43 -0500

    Revert "finshed the signup"
    This reverts commit 122919521e364c943f4b0593371166505de686ac.

--- First-Page/signin.js
+++ First-Page/signin.js
@@ -4,22 +4,23 @@ var y = document.getElementById("parents");
 var z = document.getElementById("donor");
 var a = document.getElementById("btn");
 
+var b = document.getElementById("teacher");
 
+function parent(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "-710px";
+    a.style.left = "144px";
+}
 function teacher(){
-    x.style.left = "20px";
+    x.style.left = "50px";
     y.style.left = "950px";
-    z.style.left = "900px";
+    z.style.left = "-900px";
     a.style.left = "0px";
 }
-function parent(){
-    y.style.left = "20px";
-    x.style.left = "700px";
-    z.style.left = "910px";
-    a.style.left = "180px";
-}
 function donor(){
-    z.style.left = "20px";
-    x.style.left = "800px";
+    x.style.left = "-400px";
     y.style.left = "950px";
-    a.style.left = "360px";
+    z.style.left = "50px";
+    a.style.left = "280px";
 }
